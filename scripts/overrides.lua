f7a5163d0b80a7d14c98ce0e6838b6cff7ceefaa
cheekyrotter <george0406@outlook.com>
2025-05-17 20:50:50 +0100

    lucky cards.
    lucky cards are the ONLY thing in the game to not use odds in their chance calculations, spent ages learning patches to fix this which will probably be redundant next SMODs update

--- scripts/overrides.lua
+++ scripts/overrides.lua
@@ -6,11 +6,13 @@ function pseudorandom(seed)
     local result = _pseudorandom(seed)
 
     local odds = nil
+    local extra_odds = nil
     local self_ref = nil
 
     -- Look through local variables of the original function
     for i = 1, 10 do
         local name, value = debug.getlocal(2, i)
+        -- print(name, value)
         if name == "self" then
             self_ref = value
             break
@@ -18,7 +20,21 @@ function pseudorandom(seed)
     end
 
     if self_ref and self_ref.ability and self_ref.ability.extra then
-        odds = G.GAME.probabilities.normal / self_ref.ability.extra.odds
+        local success = pcall(function()
+            extra_odds = self_ref.ability.extra.odds
+        end)
+        if not success then
+            local success = pcall(function ()
+                extra_odds = self_ref.ability.extra.odds_1
+            end)
+        end
+        if not success then
+            local success = pcall(function ()
+                extra_odds = self_ref.ability.extra
+            end)
+        end
+
+        odds = G.GAME.probabilities.normal / extra_odds
         if result >= odds then
             print("Ability failed for seed:", seed, "result:", result, "threshold:", odds)
             SMODS.calculate_context{probability_failed = true}
