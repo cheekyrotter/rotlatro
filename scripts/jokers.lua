397c1ffdd56bd83e045f14100596a964505fc7c8
cheekyrotter <george0406@outlook.com>
2025-05-23 22:32:16 +0100

    rewrite
    i rewrote the entirety of the trade deal logic to work with a loop instead of a nested table

--- scripts/jokers.lua
+++ scripts/jokers.lua
@@ -51,6 +51,7 @@ SMODS.Joker {
 SMODS.Joker { 
     -- This is the first actual joker in the mod
     -- The code for this joker is from GARBSHIT, the inspiration behind this mod
+    -- i fucking love garbshit
     -- https://github.com/Gainumki/GARBSHIT/blob/main/scripts/jokers.lua
     -- Gives 2X per Heart King
 
@@ -471,15 +472,24 @@ SMODS.Joker {
     -- Gives x1.5, -$1, cost increases with more triggers
 
     -- this genuinely might be the worst thing ive ever created
+    -- update: i
+    -- update 2: okay so i am beginning to hate this thing with a burning passion, i have sunk so much time into this for one fucking stupid joker 
+    -- i want it to die and burn in hell for all eternity for the impact it has had on my mental health
+    -- not even my ex girlfriend could give me this much trauma
+
+    -- Thank you Aurelius7309, John S Mods himself
+    -- Explained this to me in detail, incredibly nice and helpful of him
+    -- Also thank you to nh6574 for giving the nested extra tables suggestion
+    -- While I did not use this solution ultimately, they were very helpful regardless
 
     -- Key
     key = 'trade',
 
     -- Vars
-    config = { extra = { xmult = 1.5, dollars = 1, triggers = 0, reset_triggers = 5} },
+    config = { extra = { xmult = 1.5, cost = 1, triggers = 0, reset_triggers = 5} },
 
     loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.xmult, card.ability.extra.dollars, card.ability.extra.triggers, card.ability.extra.reset_triggers, card.ability.extra.reset_triggers - card.ability.extra.triggers } }
+        return { vars = { card.ability.extra.xmult, card.ability.extra.cost, card.ability.extra.triggers, card.ability.extra.reset_triggers, card.ability.extra.reset_triggers - card.ability.extra.triggers } }
     end,
 
     -- Atlas
@@ -498,76 +508,111 @@ SMODS.Joker {
     calculate = function(self, card, context)
 
         if context.final_scoring_step then
-            local triggers = 0
-            local money = to_big(G.GAME.dollars)
 
-            local relative_triggers = card.ability.extra.triggers
-            local relative_cost = card.ability.extra.dollars
+            local current_mult = mult
+            local current_money = G.GAME.dollars
+            local current_cost = card.ability.extra.cost
+            local current_triggers = card.ability.extra.triggers
 
-            if money - relative_cost >= to_big(G.GAME.bankrupt_at) then
-                repeat
-                    relative_triggers = relative_triggers + 1
-                    if relative_triggers == card.ability.extra.reset_triggers then
-                        relative_triggers = 0
-                        relative_cost = relative_cost + 1
+            repeat
+                if to_big(current_money) - to_big(current_cost) >= to_big(G.GAME.bankrupt_at) then
+
+                    current_mult = current_mult * card.ability.extra.xmult
+                    current_money = current_money - current_cost
+                    current_triggers = current_triggers + 1
+
+                    if current_triggers == card.ability.extra.reset_triggers then
+                        print("reset")
+                        current_triggers = 0
+                        current_cost = current_cost + 1
+                        SMODS.calculate_effect({message = "Price Increase!"}, card)
                     end
-                    if money - relative_cost >= to_big(G.GAME.bankrupt_at) then
-                        -- 
-                        triggers = triggers + 1
-                        money = money - relative_cost
-                        
-                    else
-                        break
-                    end
-                until((hand_chips * mult * (card.ability.extra.xmult ^ triggers)) >= G.GAME.blind.chips)
 
-                local return_table = {
-                    xmult = card.ability.extra.xmult,
-                    dollars = -card.ability.extra.dollars,
-                    
-                }
+                    SMODS.calculate_effect({
+                        xmult = card.ability.extra.xmult,
+                        dollars = -current_cost
+                    }, card)
 
-                local reset_table = {
-                    message = "Higher Prices!",
-                    func = function()
-                        card.ability.extra.dollars = card.ability.extra.dollars + 1
-                    end
-                }
-                
-                -- Thank you Aurelius7309, John S Mods himself
-                -- Explained this to me in detail, incredibly nice and helpful of him
-                -- Also thank you to nh6574 for giving the nested extra tables suggestion
-
-                local head = return_table
-                local copy = SMODS.shallow_copy(return_table)
-                
-
-                for i = 1, triggers - 1 do
-                    card.ability.extra.triggers = card.ability.extra.triggers + 1
-                    if card.ability.extra.triggers == card.ability.extra.reset_triggers then
-                        card.ability.extra.triggers = 0
-                        return_table.extra = SMODS.shallow_copy(reset_table)
-                        return_table = return_table.extra
-                        copy.dollars = copy.dollars - 1
-                    end
-                    return_table.extra = SMODS.shallow_copy(copy)
-                    return_table = return_table.extra
+                else
+                    break
                 end
+            until((current_mult * mult) >= G.GAME.blind.chips)
+            card.ability.extra.triggers = current_triggers
+            card.ability.extra.cost = current_cost
 
-                return_table.func = function()
-                        G.E_MANAGER:add_event(Event({
-                            func = function()
-                                G.GAME.dollar_buffer = 0
-                                return true
-                            end
-                    }))
-                end
 
-                G.GAME.dollar_buffer = (G.GAME.dollar_buffer or 0) - (triggers * card.ability.extra.dollars)
-
-                return head
-            end
         end
+        -- SMODS.calculate_effect({message = "idk"}, card)
+
+        -- if context.final_scoring_step then
+        --     local triggers = 0
+        --     local money = to_big(G.GAME.dollars)
+
+        --     local relative_triggers = card.ability.extra.triggers
+        --     local relative_cost = card.ability.extra.dollars
+
+        --     if money - relative_cost >= to_big(G.GAME.bankrupt_at) then
+        --         repeat
+        --             relative_triggers = relative_triggers + 1
+        --             if relative_triggers == card.ability.extra.reset_triggers then
+        --                 relative_triggers = 0
+        --                 relative_cost = relative_cost + 1
+        --             end
+        --             if money - relative_cost >= to_big(G.GAME.bankrupt_at) then
+        --                 -- 
+        --                 triggers = triggers + 1
+        --                 money = money - relative_cost
+                        
+        --             else
+        --                 break
+        --             end
+        --         until((hand_chips * mult * (card.ability.extra.xmult ^ triggers)) >= G.GAME.blind.chips)
+
+        --         local return_table = {
+        --             xmult = card.ability.extra.xmult,
+        --             dollars = -card.ability.extra.dollars,
+                    
+        --         }
+
+        --         local reset_table = {
+        --             message = "Higher Prices!",
+        --             func = function()
+        --                 card.ability.extra.dollars = card.ability.extra.dollars + 1
+        --             end
+        --         }
+                
+        --         
+
+        --         local head = return_table
+        --         local copy = SMODS.shallow_copy(return_table)
+                
+
+        --         for i = 1, triggers - 1 do
+        --             card.ability.extra.triggers = card.ability.extra.triggers + 1
+        --             if card.ability.extra.triggers == card.ability.extra.reset_triggers then
+        --                 card.ability.extra.triggers = 0
+        --                 return_table.extra = SMODS.shallow_copy(reset_table)
+        --                 return_table = return_table.extra
+        --                 copy.dollars = copy.dollars - 1
+        --             end
+        --             return_table.extra = SMODS.shallow_copy(copy)
+        --             return_table = return_table.extra
+        --         end
+
+        --         return_table.func = function()
+        --                 G.E_MANAGER:add_event(Event({
+        --                     func = function()
+        --                         G.GAME.dollar_buffer = 0
+        --                         return true
+        --                     end
+        --             }))
+        --         end
+
+        --         G.GAME.dollar_buffer = (G.GAME.dollar_buffer or 0) - (triggers * card.ability.extra.dollars)
+
+        --         return head
+        --     end
+        -- end
     end
     
 }
